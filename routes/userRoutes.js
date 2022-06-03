d1e2493fba4eb045db38b3cad19189c2ab8861bd
jhoangqm <jimmyhoangmtl@gmail.com>
2022-06-03 22:20:21 +0000

    Add userRoutes and change path on server.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,20 +5,19 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
+    const user_id = req.params.user_id;
+    db.query(`SELECT * FROM users;`, [user_id])
+      .then((data) => {
         const users = data.rows;
-        res.json({ users });
+        res.json(users, user_id);
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
       });
   });
   return router;
