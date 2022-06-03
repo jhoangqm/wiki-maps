2474cd03a236bee53a9844ba6c78533531d421fd
jhoangqm <jimmyhoangmtl@gmail.com>
2022-06-03 18:35:33 +0000

    Add DB tables to their own respective files

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,10 @@
 -- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
+
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
-);
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  location point NOT NULL
+)
---
+++ db/schema/02_pins.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS pins CASCADE;
+
+CREATE TABLE pins (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  image_url VARCHAR(255),
+  location point NOT NULL
+);
