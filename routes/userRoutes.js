1d1378ba75ad43f657e54ad7aeafff680b290503
jhoangqm <jimmyhoangmtl@gmail.com>
2022-06-05 15:46:33 +0000

    Additional functionality to userRoutes /login

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,6 +13,43 @@ const router = express.Router();
 // All SQL has been tested via psql and they work,
 // routes might not work because they haven't been tested
 module.exports = (db) => {
+  // GET login info
+  router.get("/login", (req, res) => {
+    const user_id = req.session.user_id;
+    if (user_id === undefined || !user_id) {
+      return res.send({ message: "Please login" });
+    }
+    res.send({ message: "User already logged in" });
+  });
+
+  // POST login method
+  router.post("/login", (req, res) => {
+    const { username, password } = req.body;
+    const queryString = `SELECT * FROM users WHERE username = $1;`;
+    db.query(queryString, [username, password])
+      .then((data) => {
+        const user = data.rows[0];
+        if (!user) {
+          return res
+            .status(400)
+            .send({ message: "Username not found in database" });
+        }
+
+        const validPassword = bcrypt.compareSync(password, user.password);
+
+        if (!validPassword) {
+          return res
+            .status(400)
+            .send({ message: "Password does not match username" });
+        }
+        req.session.user_id = user.id;
+        res.send({ message: "Login succesfully" });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
   // GET the current user
   router.get("/me", (req, res) => {
     const user_id = req.session.user_id;
@@ -23,7 +60,7 @@ module.exports = (db) => {
     db.query(queryString, [user_id])
       .then((data) => {
         const currUser = data.rows[0];
-        res.json(currUser);
+        res.json({ currUser });
       })
       .catch((err) => {
         res.status(500).json({ error: err.message });
