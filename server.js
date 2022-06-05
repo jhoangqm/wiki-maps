9a7801d7c3807369c3f49b960ad7dda3b4b1aa4e
jhoangqm <jimmyhoangmtl@gmail.com>
2022-06-05 23:50:45 +0000

    Comment out three routes because they're getting tested later on

--- server.js
+++ server.js
@@ -48,17 +48,19 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/userRoutes");
 const mapsRoutes = require("./routes/mapRoutes");
-const pinsRoutes = require("./routes/pinRoutes");
-const mapPinsRoutes = require("./routes/mapPinsRoutes");
-const favMapsRoutes = require("./routes/favMapsRoutes");
+// const pinsRoutes = require("./routes/pinRoutes");
+// const mapPinsRoutes = require("./routes/mapPinsRoutes");
+// const favMapsRoutes = require("./routes/favMapsRoutes");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
+
 app.use("/api/user", usersRoutes(db));
 app.use("/api/maps", mapsRoutes(db));
-app.use("/api/pins", pinsRoutes(db));
-app.use("/api/mapPins", mapPinsRoutes(db));
-app.use("/api/favMaps", favMapsRoutes(db));
+// app.use("/api/pins", pinsRoutes(db));
+// app.use("/api/mapPins", mapPinsRoutes(db));
+// app.use("/api/favMaps", favMapsRoutes(db));
+
 // Note: mount other resources here, using the same pattern above
 
 // Home page
