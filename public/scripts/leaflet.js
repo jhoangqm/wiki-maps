9315ce1e842a870e7ad14af55a0cafa414d12b00
jhoangqm <jimmyhoangmtl@gmail.com>
2022-06-06 23:54:42 +0000

    Add basic pins functionality

--- public/scripts/leaflet.js
+++ public/scripts/leaflet.js
@@ -1,15 +1,37 @@
 $(function () {
+  // calling map function
+  let map = implementMap();
+
+  // generates pins on click
+
+  let markArr = [];
+  map.on("click", (event) => {
+    let marker = L.marker([event.latlng.lat, event.latlng.lng]).addTo(map);
+    markArr.push(marker);
+    console.log(markArr);
+  });
+
+  var popup = L.popup();
+
+  function onMapClick(e) {
+    popup
+      .setLatLng(e.latlng)
+      .setContent("You saved this location", e.latlng)
+      .openOn(map);
+  }
+
+  map.on("click", onMapClick);
+});
+
+// Function that generates a map
+const implementMap = () => {
+  // generates a map
   const map = L.map("map").setView([43.6478463, -79.3807361], 16);
 
+  // tile layer retrieved from OpenStreetMap
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution: "© OpenStreetMap",
   }).addTo(map);
-
-  // let markersArray = [];
-  // map.on("click", (event) => {
-  //   var marker = L.marker([event.latlng.lat, event.latlng.lng]).addTo(map);
-  //   markersArray.push(marker);
-  //   console.log(markersArray);
-  // });
-});
+  return map;
+};
