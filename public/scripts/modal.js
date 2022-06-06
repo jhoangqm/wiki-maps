c05d892d1d991b8ad7c0b1f2f3c8d3c5bf00e08d
Mohamed <mhassankd35@gmail.com>
2022-06-06 18:58:38 -0500

    fixes login/register modals opening and closing and refactors eventlisteners to separate js file

---
+++ public/scripts/modal.js
@@ -0,0 +1,38 @@
+ //  Close Login Modal 
+ function closeLoginModal() {
+    document.querySelector('.login-modal').style.display = 'none';
+    document.querySelector('.popup-overlay').style.display = 'none';
+  }
+
+//    Close Register Modal 
+  function closeRegisterModal() {
+    document.querySelector('.register-modal').style.display = 'none';
+    document.querySelector('.popup-overlay').style.display = 'none';
+  }
+//   Open Login Modal 
+  function openLoginModal() {
+    document.querySelector('.login-modal').style.display = 'block';
+    document.querySelector('.popup-overlay').style.display = 'block';
+  }
+//   Open Register Modal 
+  function openRegisterModal() {
+    document.querySelector('.register-modal').style.display = 'block';
+    document.querySelector('.popup-overlay').style.display = 'block';
+  }
+
+  
+
+//  Close Login Event Listener
+document.querySelector('.close-login').addEventListener('click', closeLoginModal)
+
+//  Close Register Event Listener
+document.querySelector('.close-register').addEventListener('click', closeRegisterModal)
+
+//  Open Login Event Listener
+document.getElementById('login-btn').addEventListener('click', openLoginModal);
+
+// Open Register Event Listener
+document.getElementById('register-btn').addEventListener('click', openRegisterModal)
+
+
+
