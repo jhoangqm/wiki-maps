24f9e6022608620639f180722cee5149d7565846
vagrant <educastroa>
2022-06-07 17:37:24 +0000

    minor database fix

--- db/seeds/03_maps.sql
+++ db/seeds/03_maps.sql
@@ -1,4 +1,5 @@
 INSERT INTO maps (owner_id, name, latitude, longitude)
 VALUES
 (1, 'Downtown Toronto', 43.6425701, -79.3892455),
-(2, 'Mount Royal', 45.5071491, -73.725108);
+(2, 'Mount Royal', 45.5071491, -73.725108),
+(1, 'Calgary', 51.0486, -114.0708);
