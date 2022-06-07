9df22c4e00848ab87ce1514d2416d234f7d5b827
jhoangqm <jimmyhoangmtl@gmail.com>
2022-06-07 18:50:34 +0000

    Generates a marker on click, and open form that asks for a name description and imgurl

--- public/scripts/leaflet.js
+++ public/scripts/leaflet.js
@@ -26,49 +26,47 @@ $(function () {
 
 const createPins = () => {
   let markArr = [];
-  map.on("click", (event) => {
-    let marker = L.marker([event.latlng.lat, event.latlng.lng]).addTo(map);
+  window.map.on("click", (event) => {
+    let marker = L.marker([event.latlng.lat, event.latlng.lng]);
+    window.map.addLayer(marker);
     markArr.push(marker);
+    marker.bindPopup(renderPins()).openPopup();
     console.log(markArr);
   });
 
-  var popup = L.popup();
+  // var popup = L.popup();
 
-  function onMapClick(e) {
-    popup
-      .setLatLng(e.latlng)
-      .setContent("You saved this location", e.latlng)
-      .openOn(map);
-  }
+  // function onMapClick(e) {
+  //   popup
+  //     .setLatLng(e.latlng)
+  //     .setContent("You saved this location", e.latlng)
+  //     .openOn(map);
+  // }
 
-  map.on("click", onMapClick);
+  // map.on("click", onMapClick);
 };
 
-// This will be added later once I figure out the save pin to DB situation
-// const renderPins = () => {
-//   renderPins();
-// };
-
-// //Pin form that can be called when rendering a marker
-// const $pinForm = `
-//   <form class="pinForm">
-//   <input type="hidden" name="owner_id" value="#" />
-//   <label for="title">Pin Name:</label><br><br>
-//   <input type="text" name="title" id="name" placeholder="New Pin" /><br><br>
-//   <label for="description">Description:</label><br><br>
-//   <textarea type="text" name="description" placeholder="description" /><br><br>
-//   <label for="img_url">Image URL:</label><br><br>
-//   <input type="text" name="image_url" id="image" placeholder="image url" /><br><br>
-//   <p class="submit_popup">Click the place on the map where you want to add a pin.</p>
-//   <label for="latitude" class="pinlat" hidden></label><br>
-//   <input type="text" class="pinlat" name="latitude" hidden />
-//   <label for="longitude" class="pinlng" hidden></label><br>
-//   <input type="text" class="pinlng" name="longitude" hidden/>
-//   <input type="text" id="form-map-id" name="map_id" hidden />
-//   <button class="submit_popup" type="submit" hidden>submit</button>
-//   <button class="cancel" type="cancel">cancel</button>
-//   </form>
-//   `;
+//Pin form that can be called when rendering a marker
+const renderPins = () => {
+  const $pinForm = `
+  <div class="pin-form-container">
+  <form class="pin-form">
+    <label for="title"> Pin Name:</label><br>
+    <input type="text" name="title" id="name" placeholder="New Pin"/><br>
+    <input type='text' name="description" placeholder="description"/><br>
+    <input type="text" name="image_url" id="image" placeholder="image url" /><br>
+    <label for="latitude" class="pinlat" hidden></label><br>
+    <input type="text" class="pinlat" name="latitude" hidden />
+    <label for="longitude" class="pinlng" hidden></label><br>
+    <input type="text" class="pinlng" name="longitude" hidden/>
+    <input type="text" id="form-map-id" name="map_id" hidden />
+    <button class="submit_popup" type="submit">submit</button>
+    <button class="cancel" type="cancel">cancel</button>
+  </form>
+</div>
+    `;
+  return $pinForm;
+};
 
 // Function that generates a map
 const implementMap = () => {
